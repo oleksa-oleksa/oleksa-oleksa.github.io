61c1d068458d7de47b57aa80f3488e2ca8c496d4
Oleksandra Baga <oleksandra.baga@gmail.com>
2017-07-03 15:07:27 +0200

    temporary js host

--- geolocation_js/location.js
+++ geolocation_js/location.js
@@ -2,9 +2,9 @@
       // prompted by your browser. If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
       // locate you.
-      var beuth = new google.maps.LatLng(52.5453206, 13.3546582);
       var map, infoWindow;
       function initMap() {
+        var beuth = new google.maps.LatLng(52.5453206, 13.3546582);
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 52.5, lng: 13.4},
           zoom: 12
